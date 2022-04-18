2cbfccfd88867f60a6dc1bb7b25b85857ffe461c
phillycodes <82547190+phillycodes@users.noreply.github.com>
2022-04-18 01:42:57 -0400

    Add files via upload
    Gotta love a simple printf("Hello World\n!");

---
+++ MyFirstCProject/HelloWorld.c
@@ -0,0 +1,27 @@
+/*
+ * File name: HelloWorld.c
+ * Name: Philben Pierre
+ * Course: COP 2220C
+ * Facilitator: Dave Stendel
+ * Description: Program outputs Hello World!
+ * Date: 01/17/2021
+ */
+
+// Preprocessor Directives
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Name:			main()
+ * Parameters:		None.
+ * Processes:		Program outputs Hello World!
+ * Return Value:	An integer representing an error code; if the program ends without error, zero
+ *					will be returned to the calling program or operating system.
+ */
+int main() {
+
+	// Output
+	printf("Hello world!\n");
+
+	return 0;
+}
